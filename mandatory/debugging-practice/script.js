5bcb84556671d93b27aa4f64fd008d24e2c94f80
Anna Clayton-Wright <aclaytonwright@hotmail.co.uk>
2021-11-09 12:55:02 +0000

    comptete

--- mandatory/debugging-practice/script.js
+++ mandatory/debugging-practice/script.js
@@ -1,23 +1,25 @@
 let myLibrary = [];
 
-window.addEventListener("load", function (e) {
-  populateStorage();
-  render();
+window.addEventListener("load", function(e) {
+    populateStorage();
+    render();
 });
 
+
+
 function populateStorage() {
-  if (myLibrary.length == 0) {
-    let book1 = new Book("Robison Crusoe", "Daniel Defoe", "252", true);
-    let book2 = new Book(
-      "The Old Man and the Sea",
-      "Ernest Hemingway",
-      "127",
-      true
-    );
-    myLibrary.push(book1);
-    myLibrary.push(book2);
-    render();
-  }
+    if (myLibrary.length == 0) {
+        let book1 = new Book("Robinson Crusoe", "Daniel Defoe", "252", true);
+        let book2 = new Book(
+            "The Old Man and the Sea",
+            "Ernest Hemingway",
+            "127",
+            true
+        );
+        myLibrary.push(book1);
+        myLibrary.push(book2);
+        render();
+    }
 }
 
 const title = document.getElementById("title");
@@ -28,38 +30,37 @@ const check = document.getElementById("check");
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
 function submit() {
-  if (
-    title.value == null ||
-    title.value == "" ||
-    pages.value == null ||
-    pages.value == ""
-  ) {
-    alert("Please fill all fields!");
-    return false;
-  } else {
-    let book = new Book(title.value, title.value, pages.value, check.checked);
-    library.push(book);
-    render();
-  }
+    if (
+        title.value == null ||
+        title.value == "" ||
+        pages.value == null ||
+        pages.value == ""
+    ) {
+        alert("Please fill all fields!");
+        return false;
+    } else {
+        let book = new Book(title.value, title.value, pages.value, check.checked);
+        myLibrary.push(book);
+        render();
+    }
 }
 
 function Book(title, author, pages, check) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.check = check;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.check = check;
 }
 
 function render() {
-  let table = document.getElementById("display");
-  let rowsNumber = table.rows.length;
-  //delete old table
-  for (let n = rowsNumber - 1; n > 0; n-- {
-    table.deleteRow(n);
-  }
-  //insert updated row and cells
-  let length = myLibrary.length;
-  for (let i = 0; i < length; i++) {
+    let table = document.getElementById("display");
+    let rowsNumber = table.rows.length;
+    //delete old table
+    for (let n = rowsNumber - 1; n > 0; n--) table.deleteRow(n);
+}
+//insert updated row and cells
+let length = myLibrary.length;
+for (let i = 0; i < length; i++) {
     let row = table.insertRow(1);
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
@@ -77,15 +78,15 @@ function render() {
     cell4.appendChild(changeBut);
     let readStatus = "";
     if (myLibrary[i].check == false) {
-      readStatus = "Yes";
+        readStatus = "Yes";
     } else {
-      readStatus = "No";
+        readStatus = "No";
     }
     changeBut.innerHTML = readStatus;
 
-    changeBut.addEventListener("click", function () {
-      myLibrary[i].check = !myLibrary[i].check;
-      render();
+    changeBut.addEventListener("click", function() {
+        myLibrary[i].check = !myLibrary[i].check;
+        render();
     });
 
     //add delete button to every row and render again
@@ -94,10 +95,9 @@ function render() {
     cell5.appendChild(delBut);
     delBut.className = "btn btn-warning";
     delBut.innerHTML = "Delete";
-    delBut.addEventListener("clicks", function () {
-      alert(`You've deleted title: ${myLibrary[i].title}`);
-      myLibrary.splice(i, 1);
-      render();
+    delBut.addEventListener("click", function() {
+        alert(`You've deleted title: ${myLibrary[i].title}`);
+        myLibrary.splice(i, 1);
+        render();
     });
-  }
 }
